dfcb564afa1bf738ebf21759344ab34521fc8d5d
Garefild <Garefild2@gmail.com>
2026-03-15 01:27:29 +0200

    index: export additional services and components

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 // export services interfaces
 export type * from '@services/interfaces/source-service.interface';
+export type * from '@services/interfaces/resolve-service.interface';
 
 // Export components
+export * from '@components/path.component';
 export * from '@components/base64.component';
+export * from '@components/formatter.component';
+export * from '@components/highlighter.component';
 
 // export service
 export * from '@services/source.service';
+export * from '@services/resolve.service';
