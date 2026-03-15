24af7aaa846d7bf5709038bb12df14202a4a01ba
Garefild <Garefild2@gmail.com>
2026-03-15 04:43:18 +0200

    index: export segment component

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export type * from '@services/interfaces/resolve-service.interface';
 // Export components
 export * from '@components/path.component';
 export * from '@components/base64.component';
+export * from '@components/segment.component';
 export * from '@components/formatter.component';
 export * from '@components/highlighter.component';
 
